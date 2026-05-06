103213df49f9f5b7084539ef3396529a4cff2b8c
Shane <hsieh.shane@icloud.com>
2026-05-06 15:34:48 +0800

    Add: python-static-methods - submission-1

---
+++ Python OOP/python-static-methods/submission-1.py
@@ -0,0 +1,18 @@
+class CurrencyConverter:
+    rates = {  
+        'EUR': 1.20,  # 1 EUR = 1.20 USD
+        'JPY': 0.01   # 1 JPY = 0.01 USD
+    } # Class attribute
+
+    # TODO: Implement the static method `to_usd`
+    @staticmethod
+    def to_usd(num: int, given_rate: str) -> float:
+        for r, rate in CurrencyConverter.rates.items():
+            if r == given_rate:
+                return num * rate
+        print(f"No currency found in the database")
+        
+    
+
+print(f"100 EUR = {CurrencyConverter.to_usd(100, 'EUR')} USD")     # 120 USD
+print(f"100 JPY = {CurrencyConverter.to_usd(100, 'JPY')} USD")     # 1 USD
