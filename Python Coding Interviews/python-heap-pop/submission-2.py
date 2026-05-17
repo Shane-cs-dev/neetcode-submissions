01cdd37b605c4b932be15834013241308bf1f36f
Shane <hsieh.shane@icloud.com>
2026-05-17 12:55:33 +0800

    Add: python-heap-pop - submission-2

---
+++ Python Coding Interviews/python-heap-pop/submission-2.py
@@ -0,0 +1,25 @@
+import heapq
+from typing import List
+
+
+def heap_pop(heap: List[int]) -> List[int]:
+    # heapp = []
+    # for num in heap:
+    #     heapq.heappush(heapp, num)
+    
+    # ans = []
+    # while (len(heapp) != 0):
+    #     ans.append(heapq.heappop(heapp))
+
+    # return ans
+
+    heapp = heap.copy()
+    heapq.heapify(heapp)
+    return [heapq.heappop(heapp) for i in range(len(heapp))]
+
+
+
+# do not modify below this line
+print(heap_pop([1, 2, 3]))
+print(heap_pop([1, 3, 2]))
+print(heap_pop([6, 7, 8, 12, 9, 10]))
