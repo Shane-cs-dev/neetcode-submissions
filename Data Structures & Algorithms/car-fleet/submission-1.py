7ba0ca903fc4b5bc2e47dfff02f32f464ba7975f
Shane <hsieh.shane@icloud.com>
2026-07-14 23:17:07 +0800

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        # Create stack and store speed and position into it
+        stack = [(i, v) for i, v in zip(position, speed)]
+        stack.sort(key=lambda x: x[0])
+
+        # Pop from stack and check the number of the car fleet
+        res = 0
+        prev_time = 0
+        while stack:
+            # Pop current one and check the time to reach the target position from its position
+            pos, s = stack.pop()
+
+            # Calculation
+            curr_time = (target - pos) / s
+
+            # If the time needed to reach the target is larger than previous time, add one car fleet
+            if curr_time > prev_time:
+                res += 1
+                prev_time = curr_time
+            
+        return res
