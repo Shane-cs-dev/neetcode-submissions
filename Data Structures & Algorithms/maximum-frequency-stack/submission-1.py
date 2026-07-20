8b8ee2699cff3d4867bb7957f931b64811c66b7a
Shane <hsieh.shane@icloud.com>
2026-07-20 15:52:13 +0800

    Add: maximum-frequency-stack - submission-1

---
+++ Data Structures & Algorithms/maximum-frequency-stack/submission-1.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+
+class FreqStack:
+
+    def __init__(self):
+        self.heap = []
+        self.count = defaultdict(int)
+        self.index = 0
+
+    def push(self, val: int) -> None:
+        self.count[val] += 1
+        heapq.heappush(self.heap, (-self.count[val], -self.index, val))
+        self.index += 1
+
+    def pop(self) -> int:
+        count, idx, val = heapq.heappop(self.heap)
+        self.count[val] -= 1
+        return val
+
+
+# Your FreqStack object will be instantiated and called as such:
+# obj = FreqStack()
+# obj.push(val)
+# param_2 = obj.pop()
