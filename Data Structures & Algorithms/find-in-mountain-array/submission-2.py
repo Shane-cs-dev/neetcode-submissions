47a918785b31c7b934b9464be581838bf5f6a0b5
Shane <hsieh.shane@icloud.com>
2026-08-10 13:21:56 +0800

    Add: find-in-mountain-array - submission-2

---
+++ Data Structures & Algorithms/find-in-mountain-array/submission-2.py
@@ -0,0 +1,55 @@
+class Solution:
+    def findInMountainArray(self, target: int, mountainArr: 'MountainArray') -> int:
+        # Get the size of the mountainArr
+        n = mountainArr.length()
+
+        # Find the peak element inside of the mountainArr
+        # Define left and right pointer
+        left, right = 0, n - 1
+        peak_idx = -1
+        while left < right:
+            mid = left + (right - left) // 2
+
+            # Attain the val of the left, mid, right index
+            left_val, mid_val, right_val = mountainArr.get(mid - 1), mountainArr.get(mid), mountainArr.get(mid + 1)
+
+            # current position at the left side of the peak
+            if left_val < mid_val < right_val:
+                left = mid + 1
+            elif left_val > mid_val > right_val:
+                right = mid - 1
+            else:
+                peak_idx = mid
+                print(f"This is peak index: {peak_idx}, value: {mountainArr.get(peak_idx)}")
+                break
+        
+        # Search target in the left side of the array
+        left, right = 0, peak_idx
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            left_val, mid_val, right_val = mountainArr.get(left), mountainArr.get(mid), mountainArr.get(right)
+
+            if mid_val == target:
+                return mid
+            elif mid_val < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        print(f"This is left side: {left}, value: {mountainArr.get(left)}")
+        # Search target in the right side of the array
+        left, right = peak_idx + 1, n - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            left_val, mid_val, right_val = mountainArr.get(left), mountainArr.get(mid), mountainArr.get(right)
+
+            if mid_val == target:
+                return mid
+            elif mid_val > target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        print(f"This is right side: {left}")
+        return -1
+            
